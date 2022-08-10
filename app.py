0121818a8ebe38e231b5085ab4620370dc533217
Amgoyes <amgoyes@espe.edu.ec>
2022-08-09 19:57:33 -0500

    Cambio de rama

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ class MainApp(App):
         label = Label(text='Hello from Kivy',
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
-
         return label
 
 if __name__ == '__main__':
